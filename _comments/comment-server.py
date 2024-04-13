7f800957b01cddb4adc359275cd9508f0f8529fb
stuck <aireportyt@gmail.com>
2024-04-13 11:10:57 +0200

    Finish work

--- _comments/comment-server.py
+++ _comments/comment-server.py
@@ -3,11 +3,17 @@ import requests
 import yaml
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import socketserver
-import urlparse
+import logging
+import time
 
 PORT = 9090
 COMMENTS_DIR = os.path.abspath(".")
 COMMENTS_FILENAME = "%s_%s.yaml"
+
+def comments_list(directory=COMMENTS_DIR):
+    return list(
+        filter(lambda f: f.endswith(".yaml"), os.listdir(directory))
+    )
 
 class Comment:
     def make_comment_file(self):
@@ -26,22 +32,70 @@ content: |
         self.content = content
         self.reply_to = reply_to
         self.email = email
-        self.id = len(lambda f: f.endswith(".yaml"), os.listdir(COMMENTS_DIR))
+        self.id = len(comments_list())
 
     def write_comment(self):
-        self.path = os.path.join(COMMENTS_DIR, COMMENTS_FILENAME % (self.id, self.time))
-        with open(self.path, 'w') as file:
-            file.write(self.make_comments_file())
+        self.path = os.path.join(COMMENTS_DIR, self.reply_to.strip("/"), COMMENTS_FILENAME % (self.id, self.time))
+        os.makedirs(os.path.dirname(self.path), exist_ok=True)
+        print(self.make_comment_file())
+        with open(self.path, 'w+') as file:
+            file.write(self.make_comment_file())
         file.close()
+        print(f"saved to {self.path}")
 
 class CommentsServer(BaseHTTPRequestHandler):
+    def respond(self, status, status_text, body=""):
+        self.send_response(status, status_text)
+        self.end_headers()
+        self.wfile.write(body.encode('utf-8'))
+
+    def respond_OK(self, body=""):
+        self.respond(200, 'OK', body)
+
+    def get_payload(self):
+        length = int(self.headers['Content-Length'])
+        payload = self.rfile.read(length).decode('utf-8')
+        data = {}
+        for item in payload.split('&'):
+            key, value = item.split('=')
+            data[key] = value
+        return data
+
     def do_POST(self):
-        pass #gtg finish this later
+        payload = self.get_payload()
+        print(payload)
+        comment_data = payload
 
-def run():
+        for att in ['author', 'time', 'content', 'reply_to', 'email']:
+            if att not in comment_data:
+                if att in ['author', 'content']:
+                    self.respond(400, 'Missing attribute: %' % att)
+                    break
+                elif att == 'time': comment_data['time'] = time.time()
+                else: comment_data[att] = ''
+
+        comment = Comment(
+            comment_data['author'],
+            int(comment_data['time']),
+            comment_data['content'],
+            comment_data['reply_to'],
+            email=comment_data['email'],
+        )
+        comment.write_comment()
+
+def run(port=PORT):
+    logging.basicConfig(level=logging.INFO)
     print("Avvio del server...")
-    server_address = ('127.0.0.1', PORT)
+    server_address = ('', port)
     httpd = HTTPServer(server_address, CommentsServer)
-    httpd.serve_forever()
+    logging.info('Starting server...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Closing server...\n')
 
-run()
+if __name__ == '__main__':
+    from sys import argv
+    run(*argv[1:])
